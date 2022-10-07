256b24e8849aa30ed584c0e40d822355b63507bd
kakon-mehedi <kakon9000@gmail.com>
2022-10-07 08:25:47 +0600

    Profile Page Added

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import './profile.css'
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./profile.css";
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <>
+      <Topbar />
+      <div className="profile-container">
+        <Sidebar />
+
+        <div className="profile-right">
+          <div className="profile-right-top">
+            <div className="cover-photo-section">
+              <img
+                src="assets/post/3.jpeg"
+                alt="CoverPhoto"
+                className="cover-photo"
+              />
+
+              <img
+                src="assets/person/8.jpeg"
+                alt="profilePicture"
+                className="profile-picture"
+              />
+            </div>
+
+            <div className="profile-info">
+              <h4 className="profile-info-name">Kakon Mehedi</h4>
+              <p className="profile-info-text">Hello Friends </p>
+            </div>
+          </div>
+
+          <div className="profile-right-bottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Profile
+export default Profile;
